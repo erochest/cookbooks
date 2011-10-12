c1697ba7fe585c4c338ae91aa3f01694928d4755
Eric Rochester <err8n@virginia.edu>
2011-10-11 23:00:24 -0400

    Added recipe for dependencies for the LAP site.

---
+++ lap/metadata.rb
@@ -0,0 +1,11 @@
+name              'lap'
+version           '0.1'
+depends           %w{build-essential}
+description       'This downloads and installs the dependencies for the LAP site.'
+maintainer        'Eric Rochester'
+maintainer_email  'erochest@gmail.com'
+license           'Apache 2.0'
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
---
+++ lap/recipes/default.rb
@@ -0,0 +1,45 @@
+
+require_recipe 'python'
+require_recipe 'mysql::server'
+
+package 'libmysqlclient-dev'
+
+python_pip 'Flask' do
+  action :install
+end
+
+python_pip 'Flask-Compass' do
+  action :install
+end
+
+python_pip 'Flask-Coffee' do
+  action :install
+end
+
+python_pip 'MySQL-python' do
+  action :install
+end
+
+python_pip 'SQLAlchemy' do
+  action :install
+end
+
+python_pip 'Flask-SQLAlchemy' do
+  action :install
+end
+
+python_pip 'Flask-Testing' do
+  action :install
+end
+
+template '/tmp/create_lap_db.sql' do
+  source 'create_lap_db.sql.erb'
+  action :create
+end
+
+execute 'create-lap-db' do
+  command "mysql -hlocalhost -uroot -p#{node.mysql.server_root_password} mysql < /tmp/create_lap_db.sql"
+  action :run
+end
+
+
